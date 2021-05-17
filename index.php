37b1aea742a70a665d39cee7dd6c6810c3699023
Roy <b.roynaim@gmail.com>
2021-05-17 17:07:52 +0300

    updated 1

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+  class User {
+    private $name;
+    private $lastname;
+    private $date_of_birth;
+    private $date_of_join;
+  }
+
+  class premiumUser {
+    private $name;
+    private $lastname;
+    private $date_of_birth;
+    private $date_of_join;
+  }
+
+  class Products {
+
+  }
+
+  $client = new User();
+  $premium = new premiumUser();
+ ?>
