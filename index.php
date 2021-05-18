8d4c19608f6813958069737ad9345261c7cfbe31
Roy <b.roynaim@gmail.com>
2021-05-18 23:27:02 +0300

    updating

--- index.php
+++ index.php
@@ -4,12 +4,14 @@
   require_once 'CreditCard.php';
 
 
-
+  //products name with its details
   $bag = new Product ('bag', 'A Sport bag', 9.99, true, 'XX1110');
   $wallet = new Product ('wallet', 'Armani men wallet', 24.99, true, 'XX2110');
+
   $user1 = new User ('Pinko', 'Pallino', '2000-12-30', 'pinko', 'name');
   $credit_card = new CreditCard ('1234567898765', 'Visa', 'Pinko Pallino', '2025-05', '123');
 
   $user1->setCreditCard($credit_card);
 
   $user1->addShoppingCart($bag, 5);
+  $user1->addShoppingCart($wallet, 2);
